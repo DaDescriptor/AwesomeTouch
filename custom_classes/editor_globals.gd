61a0cee2ee376815c6152d586f0a47491286defd
DaDescriptor <mineconhoogy@gmail.com>
2024-03-19 09:23:01 +0700

    finished key logic, added setter for shift in editor globals

--- custom_classes/editor_globals.gd
+++ custom_classes/editor_globals.gd
@@ -1,6 +1,22 @@
-extends Object
+extends Node
 ## This class contains global variables. Wow. I've just opened America to yall.
 
+# VARIABLES
 ## Shift state. 0 = not pressed, 1 = pressed, 2 = caps locked.
-## If 1, should reset to 0 when a key is pressed.
-@export var shift_state: int = 0
+## If 1, key.gd should reset to 0 when pressed.
+@export var shift_state: int = 0 :
+	set(value):
+		if value < 0 or value > 2:
+			# gatekeeping incorrect values :)
+			push_error("value is < 0 or > 2")
+			return
+		shift_state = value
+		shift_state_changed.emit(value)
+
+## TextEdit containing the code.
+## The said TextEdit must be set on its own.
+@export var text_edit: TextEdit
+
+# SIGNALS
+## Fires when shift_state is changed
+signal shift_state_changed(new_state: int)
--- custom_classes/key.gd
+++ custom_classes/key.gd
@@ -3,17 +3,41 @@ class_name Key extends Button
 ## Automatically makes the text caps when shift is pressed.
 
 ## TextEdit that this key will insert text to.
+## If not set will use EditorGlobals.text_edit
 @export var text_edit: TextEdit
 ## A letter this key will insert.
+## If not set will use the button's text.
 @export var letter: String
 ## What to insert when shift is enabled.
-## If not set will turn `letter` into caps (if possible)
+## If not set will turn `letter` into caps.
 @export var shifted_letter: String
+## Text displayed on the button.
+## If not set will use the button's text.
+@export var noshift_text: String # really want to make a typo here
+## Text displayed on the button when shift is enabled.
+## If not set will turn `noshift_text` into caps.
+@export var shift_text: String
 
 func _pressed():
 	if text_edit == null:
-		push_error("text_edit is not set.")
+		text_edit = EditorGlobals.text_edit
+		if text_edit == null:
+			push_error("text_edit is not set. It should have a script that 
+			assigns itself to EditorGlobals.text_edit.")
 	if letter == null or letter == "":
-		push_error("letter is not set.")
+		print("letter is not set, trying to autoset..")
+		letter = text
+		print(str("letter is now set to ", letter))
 	if shifted_letter == null or shifted_letter == "":
-		print("shifted_letter is not set, trying to autoset.")
+		print("shifted_letter is not set, trying to autoset..")
+		shifted_letter = letter.to_upper()
+		print(str("shifted_letter is now set to ", shifted_letter))
+	
+	text_edit.insert_text_at_caret(letter if EditorGlobals.shift_state == 0 else shifted_letter, -1)
+	# insert letter if shift is not pressed, otherwise insert shifted_letter
+	# second argument is caret id, -1 means all carets
+	
+	if EditorGlobals.shift_state == 1:
+		# reset shift state if it was not caps locked
+		EditorGlobals.shift_state = 0
+
