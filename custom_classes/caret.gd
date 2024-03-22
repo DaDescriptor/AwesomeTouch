8f8e392f780ba7209ba2f09e6ed6d442944e5521
DaDescriptor <mineconhoogy@gmail.com>
2024-03-22 07:02:37 +0700

    custom caret finally works properly yay

---
+++ custom_classes/caret.gd
@@ -0,0 +1,35 @@
+class_name Caret extends Control
+## Acts as a caret, because default one is being hiden while unfocused.
+## dirty hack, i'm covered in shit
+
+## Controls how fast will the cursor blink, set by EditorGlobals.
+var blink_speed: float = 0.5
+## Self-explanatory.
+var blink_timer: float = blink_speed
+
+
+
+func _process(delta):
+	position = EditorGlobals.text_edit.get_caret_draw_pos() + Vector2(24, -32)
+	
+	# how the timer works:
+	# first, we set blink_timer to blink_speed
+	#  [##########*]
+	#  -1    0    1
+	# (-1 and 1 are replaced by blink_speed)
+	
+	#  [#####*#####]
+	#  -1    0    1
+	# when it reaches zero, the caret hides:
+	if blink_timer <= 0:
+		visible = false
+		
+	#  [*##########]
+	#  -1    0    1
+	# when it reaches negative blink_speed, the caret enables and timer resets:
+	if blink_timer <= -blink_speed:
+		visible = true
+		blink_timer = blink_speed
+	
+	# subtract delta from the timer so it ticks down:
+	blink_timer -= delta
--- custom_classes/keys/key_scroller.gd
+++ custom_classes/keys/key_scroller.gd
@@ -1,22 +1,62 @@
 class_name ScrollKey extends Button
-## the cool key which has the hand icon on it
-## and serves as the scroll wheel for touchscreen users
-## this editor is designed for touchscreen lol
+## the cool key which has the hand icon on it.
+## and serves as the scroll wheel for touchscreen users.
+## this editor is designed for touchscreen lol.
 
-## go one line down when this amount of pixels is scrolled
+## go one line down when this amount of pixels is scrolled.
 @export var sensitivity: float = 50.0
 
-
-## how much did the user move their cursor since holding the button
-## probably need to invert this because of godot's coordinate system
-var scrolled_amount: Vector2 = Vector2(0, 0) # stupid snake case
-## self-explanatory
+## how much did the user move their cursor since holding the button.
+## probably need to invert this because of godot's coordinate system.
+var scrolled_amount: Vector2
+## self-explanatory.
 var is_pressed: bool = false
 
-func _
+func _gui_input(event: InputEvent):
+	if event.is_pressed():
+		is_pressed = true
+	if event.is_released():
+		is_pressed = false
+		scrolled_amount = Vector2.ZERO
 
 func _process(_delta):
+	if !is_pressed:
+		print("not pressed")
+		return
 	scrolled_amount += Input.get_last_mouse_velocity()
-	# print(Input.get_last_mouse_velocity())
-	# need to check if it works with touchscreen
-	# it does
+	
+	## how much columns got scrolled.
+	## example: sensitivity is 50, scrolled_amount is (-125, 23).
+	## full_scrolls will be (-2, 0)
+	var full_scrolls: Vector2 = Vector2(
+		fit_x_into_y(sensitivity, scrolled_amount.x),
+		fit_x_into_y(sensitivity, scrolled_amount.y)
+	)
+	
+	print(str(
+		"scrolled: ", scrolled_amount, 
+		"\nsensitivity: ", sensitivity,
+		"\nfull scrolls: ", full_scrolls
+	))
+	
+	if full_scrolls.x != 0:
+		scrolled_amount.x = 0
+		EditorGlobals.text_edit.set_caret_column(main_caret_only().x+full_scrolls.x)
+	if full_scrolls.y != 0:
+		scrolled_amount.y = 0
+		EditorGlobals.text_edit.set_caret_column(main_caret_only().y+full_scrolls.y)
+
+func main_caret_only():
+	## remove all secondary carets and return main caret's position.
+	## returns Vector2(column, line)
+	# ideas for name change are welcome
+	EditorGlobals.text_edit.remove_secondary_carets()
+	return Vector2(
+		EditorGlobals.text_edit.get_caret_column(),
+		EditorGlobals.text_edit.get_caret_line()
+	)
+
+func fit_x_into_y(x, y):
+	## tries to fit as many x into y as possible.
+	## why gdscript does not have % operator
+	return floor(y/x)
